25843fa071c27363b40bac9335a0bcffd4203b5d
PRASHANT PARASHAR (CRGL-THIRDPARTY.COM) <prashant_parashar@crgl-thirdparty.com>
2021-06-05 18:08:19 +0530

    added pipeline support

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
         <span>
           <span>Learn </span>
