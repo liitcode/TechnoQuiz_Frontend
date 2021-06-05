1432543a523fc6c065fe790172b2181b2e0b7664
Divyanshu Verma <liitcode@gmail.com>
2021-06-05 21:32:42 +0530

    Tests

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <span>
-          <span>Lear </span>
+          <span>Learn</span>
           <a
             className="App-link"
             href="https://reactjs.org/"
