97db9fa561180f3659d948e19034c387a6b62367
PRASHANT PARASHAR (CRGL-THIRDPARTY.COM) <prashant_parashar@crgl-thirdparty.com>
2021-06-11 01:43:37 +0530

    login/Signup Design updates

--- src/components/Auth/SignIn/index.js
+++ src/components/Auth/SignIn/index.js
@@ -3,136 +3,143 @@
 /* eslint-disable no-shadow */
 /* eslint-disable consistent-return */
 /* eslint-disable no-underscore-dangle */
-import React , { useState,useRef } from 'react';
-import { useDispatch , useSelector } from 'react-redux';
+import React, { useState, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-
 import Form from 'react-validation/build/form';
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
-import { isEmail } from "validator";
-
+import Input from 'react-validation/build/input';
+import CheckButton from 'react-validation/build/button';
+import { isEmail } from 'validator';
+import loginImg from '../../../assets/images/login.svg';
 import { login } from '../../../actions/actionCreators/auth';
-
+import '../auth.scss';
 
 const required = (value) => {
-    if (!value) {
-        return (
-            <div className='alert alert-danger' role='alert'>
-            This field is required
-            </div>
-        );
-    }
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required
+      </div>
+    );
+  }
 };
 
-const mail = value => {
-    if (!isEmail(value)) {
-      return (
-        <div className="alert alert-danger" role="alert">
-          This is not a valid email.
-        </div>
-      );
+const mail = (value) => {
+  if (!isEmail(value)) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This is not a valid email.
+      </div>
+    );
+  }
+};
+
+const Login = (props) => {
+  const form = useRef();
+  const checkButton = useRef();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const { isLoggedin } = useSelector((state) => state.auth);
+  const { message } = useSelector((state) => state.message);
+
+  const dispatch = useDispatch();
+
+  const onChangeEmail = (e) => {
+    const email = e.target.value;
+    setEmail(email);
+  };
+
+  const onChangePassword = (e) => {
+    const password = e.target.value;
+    setPassword(password);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    form.current.validateAll();
+
+    if (checkButton.current.context._errors.length === 0) {
+      dispatch(login(email, password))
+        .then(() => {
+          props.history.push('/category');
+          window.location.reload();
+        })
+        .catch(() => {
+          setLoading(false);
+        });
+    } else {
+      setLoading(false);
     }
   };
 
-const Login = (props) => {
-    const form = useRef();
-    const checkButton = useRef();
+  if (isLoggedin) return <Redirect to="/category" />;
 
-    const [ email, setEmail ] = useState('');
-    const [ password , setPassword ] = useState('');
-    const [ loading,setLoading ] = useState(false);
-
-    const { isLoggedin } = useSelector(state => state.auth);
-    const { message } = useSelector(state => state.message);
-
-    const dispatch = useDispatch();
-    
-    const onChangeEmail = (e) => {
-        const email = e.target.value;
-        setEmail(email);
-    };
-
-    const onChangePassword = (e) => {
-        const password = e.target.value;
-        setPassword(password);
-    };
-
-    const handleLogin = (e) => {
-        e.preventDefault();
-
-        setLoading(true);
-
-        form.current.validateAll();
-
-        if (checkButton.current.context._errors.length === 0){
-            dispatch(login(email,password))
-            .then(()=>{
-                props.history.push('/category');
-                window.location.reload();
-            })
-            .catch(()=>{
-                setLoading(false);
-            });
-        } else {
-            setLoading(false);
-        }
-    };
-
-    if(isLoggedin) return <Redirect to='/category' />
-
-    return(
-        <div className='col-md-12'>
-            <div className='card card-container'>
-                <img
-                src = '//ssl.gstatic.com/accounts/ui/avatar_2x.png'
-                alt = 'profile-img'
-                className = 'profile-img-card'
-                />
-
-                <Form onSubmit={handleLogin} ref={form}>
-                    <div className='form-group'>
-                        <label htmlFor='email' >Email</label>
-                        <Input
-                            type = 'text'
-                            className = 'form-control'
-                            name = 'email'
-                            value = {email}
-                            onChange = {onChangeEmail}
-                            validations = {[required,mail]}
-                        />
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor='password' >Password</label>
-                        <Input
-                            type = 'password'
-                            className = 'form-control'
-                            name = 'password'
-                            value = {password}
-                            onChange = {onChangePassword}
-                            validations = {[required]}
-                        />
-                    </div>
-                    <div className='form-group'>
-                        <button className='btn btn-primary btn-block' disabled={loading} type='button'>
-                            {loading && (
-                            <span className="spinner-border spinner-border-sm" />
-                            )}
-                            <span>Login</span>
-                        </button>
-                    </div>
-                    {message && (
-                        <div className="form-group">
-                            <div className="alert alert-danger" role="alert">
-                            {message}
-                            </div>
-                        </div>
-                    )}
-                    <CheckButton style={{ display: "none" }} ref={checkButton} />
-                </Form>
+  return (
+    <div className="base">
+      <div className="base-container">
+        <div className="header">Login</div>
+        <div className="content">
+          <div className="image">
+            <img src={loginImg} alt="" />
+          </div>
+          <Form onSubmit={handleLogin} ref={form}>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <Input
+                type="text"
+                className="form-control"
+                name="email"
+                value={email}
+                onChange={onChangeEmail}
+                validations={[required, mail]}
+              />
             </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <Input
+                type="password"
+                className="form-control"
+                name="password"
+                value={password}
+                onChange={onChangePassword}
+                validations={[required]}
+              />
+            </div>
+            <div className="form-group">
+              <button
+                className="btn btn-primary btn-block"
+                disabled={loading}
+                type="button"
+              >
+                {loading && (
+                  <span className="spinner-border spinner-border-sm" />
+                )}
+                <span>Login</span>
+              </button>
+            </div>
+            {message && (
+              <div className="form-group">
+                <div className="alert-danger" role="alert">
+                  {message}
+                </div>
+              </div>
+            )}
+            <CheckButton
+              className="btn"
+              style={{ display: 'none' }}
+              ref={checkButton}
+            />
+          </Form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Auth/SignUp/index.js
+++ src/components/Auth/SignUp/index.js
@@ -2,169 +2,180 @@
 /* eslint-disable no-shadow */
 /* eslint-disable consistent-return */
 /* eslint-disable no-underscore-dangle */
-import React , { useState,useRef } from 'react';
-import { useDispatch , useSelector } from 'react-redux';
+import React, { useState, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-
 import Form from 'react-validation/build/form';
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
-import { isEmail } from "validator";
-
+import Input from 'react-validation/build/input';
+import CheckButton from 'react-validation/build/button';
+import { isEmail } from 'validator';
+import loginImg from '../../../assets/images/login.svg';
 import { register } from '../../../actions/actionCreators/auth';
+import '../auth.scss';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import '../auth.css';
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import '../auth.css';
 
 const required = (value) => {
-    if (!value) {
-        return (
-            <div className='alert alert-danger' role='alert'>
-            This field is required
-            </div>
-        );
-    }
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required
+      </div>
+    );
+  }
 };
 
-const mail = value => {
-    if (!isEmail(value)) {
-      return (
-        <div className="alert alert-danger" role="alert">
-          This is not a valid email.
-        </div>
-      );
-    }
-  };
+const mail = (value) => {
+  if (!isEmail(value)) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This is not a valid email.
+      </div>
+    );
+  }
+};
 
 const vusername = (value) => {
-if (value.length < 3 || value.length > 20) {
+  if (value.length < 3 || value.length > 20) {
     return (
-    <div className="alert alert-danger" role="alert">
+      <div className="alert alert-danger" role="alert">
         The username must be between 3 and 20 characters.
-    </div>
+      </div>
     );
-}
+  }
 };
 
 const vpassword = (value) => {
-if (value.length < 6 || value.length > 40) {
+  if (value.length < 6 || value.length > 40) {
     return (
-    <div className="alert alert-danger" role="alert">
+      <div className="alert alert-danger" role="alert">
         The password must be between 6 and 40 characters.
-    </div>
+      </div>
     );
-}
+  }
 };
 
 const Register = () => {
-    const form = useRef();
-    const checkButton = useRef();
+  const form = useRef();
+  const checkButton = useRef();
 
-    const [ email, setEmail ] = useState('');
-    const [ password , setPassword ] = useState('');
-    const [ username , setUsername ] = useState('');
-    const [ successful,setSuccessful ] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [successful, setSuccessful] = useState(false);
 
-    const { message } = useSelector(state => state.message);
+  const { message } = useSelector((state) => state.message);
 
-    const dispatch = useDispatch();
-    
-    const onChangeEmail = (e) => {
-        const email = e.target.value;
-        setEmail(email);
-    };
-    
-    const onChangeUsername = (e) => {
-        const username = e.target.value;
-        setUsername(username);
-    };
+  const dispatch = useDispatch();
 
-    const onChangePassword = (e) => {
-        const password = e.target.value;
-        setPassword(password);
-    };
+  const onChangeEmail = (e) => {
+    const email = e.target.value;
+    setEmail(email);
+  };
 
-    const handleRegister = (e) => {
-        e.preventDefault();
+  const onChangeUsername = (e) => {
+    const username = e.target.value;
+    setUsername(username);
+  };
 
-        setSuccessful(false);
+  const onChangePassword = (e) => {
+    const password = e.target.value;
+    setPassword(password);
+  };
 
-        form.current.validateAll();
+  const handleRegister = (e) => {
+    e.preventDefault();
 
-        if (checkButton.current.context._errors.length === 0){
-            dispatch(register(username,email,password))
-            .then(()=>{
-                setSuccessful(true);
-                <Redirect to='/category' />
-            })
-            .catch(()=>{
-                setSuccessful(false);
-            });
-        }
-    };
+    setSuccessful(false);
 
-    return(
-        <div className='col-md-12'>
-            <div className='card card-container'>
-                <img
-                src = '//ssl.gstatic.com/accounts/ui/avatar_2x.png'
-                alt = 'profile-img'
-                className = 'profile-img-card'
-                />
+    form.current.validateAll();
 
-                <Form onSubmit={handleRegister} ref={form}>
-                {!successful && (
-                    <div>
-                    <div className='form-group'>
-                        <label htmlFor='username' >Name</label>
-                        <Input
-                            type = 'text'
-                            className = 'form-control'
-                            name = 'username'
-                            value = {username}
-                            onChange = {onChangeUsername}
-                            validations = {[required,vusername]}
-                        />
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor='email' >Email</label>
-                        <Input
-                            type = 'text'
-                            className = 'form-control'
-                            name = 'email'
-                            value = {email}
-                            onChange = {onChangeEmail}
-                            validations = {[required,mail]}
-                        />
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor='password' >Password</label>
-                        <Input
-                            type = 'password'
-                            className = 'form-control'
-                            name = 'password'
-                            value = {password}
-                            onChange = {onChangePassword}
-                            validations = {[required,vpassword]}
-                        />
-                    </div>
-                    <div className="form-group">
-                       <button className="btn btn-primary btn-block" type='button'>Sign Up</button>
-                    </div>
+    if (checkButton.current.context._errors.length === 0) {
+      dispatch(register(username, email, password))
+        .then(() => {
+          setSuccessful(true);
+          <Redirect to="/category" />;
+        })
+        .catch(() => {
+          setSuccessful(false);
+        });
+    }
+  };
+
+  return (
+    <div className="base">
+      <div className="base-container">
+        <div className="header">Register</div>
+        <div className="content">
+          <div className="image">
+            <img src={loginImg} alt="" />
+          </div>
+          <Form onSubmit={handleRegister} ref={form}>
+            {!successful && (
+              <div>
+                <div className="form-group">
+                  <label htmlFor="username">Name</label>
+                  <Input
+                    type="text"
+                    className="form-control"
+                    name="username"
+                    value={username}
+                    onChange={onChangeUsername}
+                    validations={[required, vusername]}
+                  />
                 </div>
-                )}
-                {message && (
-                    <div className="form-group">
-                        <div className={ successful ? "alert alert-success" : "alert alert-danger" } role="alert">
-                        {message}
-                        </div>
-                    </div>
-                )}
-                    <CheckButton style={{ display: "none" }} ref={checkButton} />
-                </Form>
-            </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <Input
+                    type="text"
+                    className="form-control"
+                    name="email"
+                    value={email}
+                    onChange={onChangeEmail}
+                    validations={[required, mail]}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <Input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    value={password}
+                    onChange={onChangePassword}
+                    validations={[required, vpassword]}
+                  />
+                </div>
+                <div className="form-group">
+                  <button className="btn btn-primary btn-block" type="button">
+                    Sign Up
+                  </button>
+                </div>
+              </div>
+            )}
+            {message && (
+              <div className="form-group">
+                <div
+                  className={
+                    successful ? 'alert alert-success' : 'alert alert-danger'
+                  }
+                  role="alert"
+                >
+                  {message}
+                </div>
+              </div>
+            )}
+            <CheckButton
+              className="btn"
+              style={{ display: 'none' }}
+              ref={checkButton}
+            />
+          </Form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Register;
