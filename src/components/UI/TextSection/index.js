d503d0aa7603cd6d7d29c65c3d286ed3ae0dcb00
PRASHANT PARASHAR (CRGL-THIRDPARTY.COM) <prashant_parashar@crgl-thirdparty.com>
2021-06-19 15:59:49 +0530

    UI Changes

--- src/components/UI/TextSection/index.js
+++ src/components/UI/TextSection/index.js
@@ -4,37 +4,51 @@ import Typewriter from 'typewriter-effect';
 import './textsection.module.scss';
 
 function TextSection({
-    lightBg,topLine,lightText,description,
-    img,alt,imgStart
-}){
-    return (
-        <>
-        <div className={lightBg ? 'home__section' : 'home__section darkBg'}>
-            <div className='container'>
-                <div className='row home__section-row'
-                style={{display:'flex',flexDirection: imgStart === 'start'? 'row-reverse':'row'}}>
-                    <div className='col'>
-                        <div className ='home__section-text-wrapper'>
-                            <div className='top-line'>{topLine}</div>
-                            <h1 className={lightText?'heading':'heading_dark'}> <Typewriter
-              options={{
-                strings: description,
-                autoStart: true,
-                loop: true,
-              }}
-            /></h1>
-                        </div>
-                    </div>
-                    <div className='col'>
-                        <div className='home__section-img-wrapper'>
-                            <img src={img} alt={alt} className='home__section-img' />
-                        </div>
-                    </div>
-                </div>
+  headline,
+  lightBg,
+  topLine,
+  lightText,
+  description,
+  img,
+  alt,
+  imgStart,
+}) {
+  return (
+    <>
+      <div className={lightBg ? 'home__section' : 'home__section darkBg'}>
+        <div className="container">
+          <div
+            className="row home__section-row"
+            style={{
+              display: 'flex',
+              flexDirection: imgStart === 'start' ? 'row-reverse' : 'row',
+            }}
+          >
+            <div className="col">
+              <div className="home__section-text-wrapper">
+                <div className="head-line">{headline}</div>
+                <div className="top-line">{topLine}</div>
+                <h1 className={lightText ? 'heading' : 'heading_dark'}>
+                  <Typewriter
+                    options={{
+                      strings: description,
+                      autoStart: true,
+                      loop: true,
+                    }}
+                  />
+                </h1>
+              </div>
             </div>
+            <div className="col">
+              <div className="home__section-img-wrapper">
+                <img src={img} alt={alt} className="home__section-img" />
+              </div>
+            </div>
+          </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 export default TextSection;
